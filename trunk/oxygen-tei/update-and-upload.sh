73996cdefa21efd347a1b2602c601e43523347c3
sebastian.rahtz@gmail.com <sebastian.rahtz@gmail.com@5d593ab8-aef3-fc23-1a01-c11576059531>
2012-06-18 20:34:37 +0000

    generalize script

--- trunk/oxygen-tei/update-and-upload.sh
+++ trunk/oxygen-tei/update-and-upload.sh
@@ -3,9 +3,41 @@
 # Grab the latest TEIP5 and XSL builds from Jenkins, rebuild
 # the distribution, and upload to Google for release
 #
+die()
+{
+    echo; echo
+    echo "ERROR: $@."
+    D=`date "+%Y-%m-%d %H:%M:%S"`
+    echo "$D. That was a fatal error"
+    exit 1
+}
+
 JENKINS=http://tei.oucs.ox.ac.uk/jenkins
-TEIVERSION=2.1.0
-XSLVERSION=6.12
+TEIVERSION=
+XSLVERSION=
+while test $# -gt 0; do
+  case $1 in
+    --teiversion=*)   TEIVERSION=`echo $1 | sed 's/.*=//'`;;
+    --xslversion=*)   XSLVERSION=`echo $1 | sed 's/.*=//'`;;
+   *) if test "$1" = "${1#--}" ; then 
+	   break
+	else
+	   echo "WARNING: Unrecognized option '$1' ignored"
+	   echo "For usage syntax issue $0 --help"
+	fi ;;
+  esac
+  shift
+done
+if [ -z $TEIVERSION ] 
+then
+ echo You must use the --teiversion option to specify which version of TEI P5  you are installing
+ exit 1
+fi
+if [ -z $XSLVERSION ] 
+then
+ echo You must use the --xslversion option to specify which version of TEI Stylesheets  you are installing
+ exit 1
+fi
 curl -o tei.zip $JENKINS/job/TEIP5/lastSuccessfulBuild/artifact/tei-$TEIVERSION.zip
 curl -o xsl.zip $JENKINS/job/Stylesheets/lastSuccessfulBuild/artifact/tei-xsl-$XSLVERSION.zip
 cd frameworks/tei
@@ -23,5 +55,5 @@ cd ../..
 rm tei.zip xsl.zip
 ant
 mv dist/tei.zip tei-$TEIVERSION.zip
-python googlecode_upload.py -s "TEI release $TEIVERSION and XSL $XSLVERSION" -p oxygen-tei -w wH7AD7BY7mS2 tei-$TEIVERSION.zip   
+python googlecode_upload.py -s "TEI release $TEIVERSION and XSL $XSLVERSION" -p oxygen-tei  tei-$TEIVERSION.zip   
 
