c5bc90f7007dc392f7db76aec07f7f2abffc7c0b
sebastian.rahtz@gmail.com <sebastian.rahtz@gmail.com@5d593ab8-aef3-fc23-1a01-c11576059531>
2012-06-19 13:13:07 +0000

    throw away more dross, not needed in oXygen

--- trunk/oxygen-tei/update-and-upload.sh
+++ trunk/oxygen-tei/update-and-upload.sh
@@ -38,9 +38,12 @@ then
  echo You must use the --xslversion option to specify which version of TEI Stylesheets  you are installing
  exit 1
 fi
-curl -o tei.zip $JENKINS/job/TEIP5/lastSuccessfulBuild/artifact/tei-$TEIVERSION.zip
-curl -o xsl.zip $JENKINS/job/Stylesheets/lastSuccessfulBuild/artifact/tei-xsl-$XSLVERSION.zip
+echo Download $JENKINS/job/TEIP5/lastSuccessfulBuild/artifact/tei-$TEIVERSION.zip
+curl -s -o tei.zip $JENKINS/job/TEIP5/lastSuccessfulBuild/artifact/tei-$TEIVERSION.zip
+echo Download $JENKINS/job/Stylesheets/lastSuccessfulBuild/artifact/tei-xsl-$XSLVERSION.zip
+curl -s -o xsl.zip $JENKINS/job/Stylesheets/lastSuccessfulBuild/artifact/tei-xsl-$XSLVERSION.zip
 cd frameworks/tei
+echo zap any old versions
 rm -rf xml/tei/stylesheet
 rm -rf xml/tei/odd
 rm -rf xml/tei/schema
@@ -48,15 +51,26 @@ rm -rf xml/tei/custom/schema
 rm -rf xml/tei/custom/odd
 rm -rf xml/tei/Test 
 rm -rf xml/tei/xquery
+echo unpack new files
 unzip -o -q ../../tei.zip
 unzip -o -q ../../xsl.zip
+echo remove unwanted material
 rm -rf doc
 rm -rf xml/tei/Test 
 rm -rf xml/tei/xquery
+rm -rf xml/tei/odd/Source
+rm -rf xml/tei/odd/Utilities
+rm -rf xml/tei/odd/webnav
+rm -rf xml/tei/odd/ReleaseNotes
+rm -rf xml/tei/odd/p5odds-examples.*
 cd ../..
+echo add Brown specifics
 unzip brown
-rm tei.zip xsl.zip
+rm -f tei.zip xsl.zip dist/tei.zip
+echo do Ant build
 ant
+echo move result to tei-$TEIVERSION-$XSLVERSION.zip
 mv dist/tei.zip tei-$TEIVERSION-$XSLVERSION.zip
+echo upload to Google
 python googlecode_upload.py -s "TEI release $TEIVERSION and XSL $XSLVERSION" -p oxygen-tei  tei-$TEIVERSION-$XSLVERSION.zip   
 
